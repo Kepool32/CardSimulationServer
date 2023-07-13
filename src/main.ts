48d0f612b87d9747da450cd5f646524df6a9e61f
Kepool32 <109909825+Kepool32@users.noreply.github.com>
2023-07-13 22:06:21 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(5000);
+    await app.listen(process.env.PORT ||5000);
 }
 
 bootstrap();
+
